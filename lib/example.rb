9347bb4c03b155175f33080cd8263dd8677f6b87
Deanna Kelley <deanna.georges@gmail.com>
2019-10-08 01:15:18 +0000

    Automatically backed up by Learn

--- lib/example.rb
+++ lib/example.rb
@@ -1,11 +1,11 @@
 # Use this space to code along with the readme
-
-phrase = "Hello World!"
-puts phrase
-puts phrase
-puts phrase
-puts phrase
-puts phrase
+puts say_hello_ten_times
+  phrase = "Hello World!"
+  puts phrase
+  puts phrase
+  puts phrase
+  puts phrase
+  puts phrase
 puts phrase
 puts phrase
 puts phrase
